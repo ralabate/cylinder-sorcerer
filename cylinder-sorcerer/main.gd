111a2637054433c958201879a2c7f8be350f5b06
Nicholas Charles Ralabate <ncr@blakelock.local>
2026-03-24 11:35:49 -0700

    rewrite input refactor baddie struct

--- cylinder-sorcerer/main.gd
+++ cylinder-sorcerer/main.gd
@@ -1,93 +1,150 @@
 extends Node3D
 
-const PLAYER_SPEED = 0.06
-const PLAYER_RADIUS = 0.5
-const PLAYER_SIZE = 1.0
-const PLAYER_ATTACK_ROOT_MOTION = 1.0
-
-const PLAYER_SWORD_FRAMES = 17
-const PLAYER_SWORD_ATTACK_START_ANGLE = -PI/2.0 - 0.5
-const PLAYER_SWORD_ATTACK_ANGLE_INCREMENT = 2*PI / PLAYER_SWORD_FRAMES
-const PLAYER_SWORD_LENGTH = 1.2
-const PLAYER_SWORD_DEBUG_SPHERE_SIZE = 0.15
+const PLAYER_COLOR = Color.DARK_OLIVE_GREEN
 const PLAYER_HP = 4
-const PLAYER_HURT_FRAMES = 10
+const PLAYER_NUM_HURT_FRAMES = 12
+const PLAYER_RADIUS = 0.5
+const PLAYER_SPEED = 0.04
 
-const BADDIE_COUNT = 10
-const BADDIE_SPEED = 0.02
-const BADDIE_RADIUS = 0.7
-const BADDIE_HURT_FRAMES = 14
-const BADDIE_HP = 3
+const PLAYER_SWORD_ROOT_MOTION = 2.5
+const PLAYER_NUM_SWORD_FRAMES = 20
+const PLAYER_SWORD_ATTACK_START_ANGLE = -PI/2.0 - 0.5
+const PLAYER_SWORD_ATTACK_ANGLE_INCREMENT = 2*PI / PLAYER_NUM_SWORD_FRAMES
+const PLAYER_SWORD_LENGTH = 1.5
+const PLAYER_SWORD_DEBUG_SPHERE_SIZE = 0.13
 
-const CAMERA_SHAKE_FRAMES = BADDIE_HURT_FRAMES + 3
-const CAMERA_SHAKE_AMOUNT = 0.25
+const SKULL_COUNT = 10
+
+const SKULL_COLOR = Color.DIM_GRAY
+const SKULL_HP = 3
+const SKULL_NUM_HURT_FRAMES = 14
+const SKULL_RADIUS = 1.0
+const SKULL_SPEED = 0.02
+
+const BAT_COUNT = 20
+
+const BAT_COLOR = Color.BLACK
+const BAT_HP = 1
+const BAT_NUM_HURT_FRAMES = 14
+const BAT_RADIUS = 0.2
+const BAT_SPEED = 0.01
+
+const CAMERA_SHAKE_FRAMES = SKULL_NUM_HURT_FRAMES * 2.0
+const CAMERA_SHAKE_AMOUNT = 0.35
 
 const WRAP_X = 17
 const WRAP_Z = 9
 
+enum Direction {
+	LEFT   = 0b00000001,
+	RIGHT  = 0b00000010,
+	UP     = 0b00000100,
+	DOWN   = 0b00001000,
+	SWORD  = 0b00010000,
+}
+
+var input_bit_field: int
+var input_bit_field_previous_frame: int # AFTER FOUR-WAY
 var was_attacking: bool
 var is_attacking: bool
-var is_moving_left: bool
-var is_moving_right: bool
-var is_moving_up: bool
-var is_moving_down: bool
 
 var frame: int
 var sword_frame: int
 var camera_shake_frame: int
-# hurt_frame in baddie struct
 
-var player: Character
-var baddie: Character
-var baddie_list: Array[Character]
+var player: Actor
+var baddie_list: Array[Actor]
 var sword_pivot: Node3D
 var sword: MeshInstance3D
 var sword_debug: MeshInstance3D
 var camera: Camera3D	
 
-class Character:
-	
-	var pos: Vector3
-	var theta: float
-	var radius: float
+class Actor:
+	var behavior_func
 	var color: Color
-	var mesh_instance: MeshInstance3D
-	var hurt_frame: int
+	var dir: Vector3i
 	var hp: int
-	var seed: float
+	var hurt_frame: int
 	var knockback_dir: Vector3
+	var mesh_instance: MeshInstance3D
+	var num_hurt_frames: int
+	var pos: Vector3
+	var radius: float
+	var seed: float
 	var speed: float
 	
-	func _init(p_pos: Vector3, p_radius: float, p_color: Color, p_hp: int, p_hurt_frames: int):
-		self.pos = p_pos
-		self.theta = 0
-		self.radius = p_radius
-		self.color = p_color
+	func _init(p_breed: Breed):
+		self.behavior_func = p_breed.behavior_func
+		self.dir = Vector3i(0, 0, 0)
+		self.hp = p_breed.hp
+		self.hurt_frame = p_breed.num_hurt_frames + 1
 		self.mesh_instance = MeshInstance3D.new()
 		self.mesh_instance.mesh = SphereMesh.new()
-		self.mesh_instance.mesh.radius = p_radius
-		self.mesh_instance.mesh.height = 2.0 * p_radius
+		self.mesh_instance.mesh.height = 2.0 * p_breed.radius
+		self.mesh_instance.mesh.radius = p_breed.radius
 		var material = StandardMaterial3D.new()
-		material.albedo_color = p_color
+		material.albedo_color = p_breed.color
 		material.flags_unshaded = true
 		self.mesh_instance.mesh.surface_set_material(0, material)
-		self.hurt_frame = p_hurt_frames + 1
-		self.hp = p_hp
+		self.num_hurt_frames = p_breed.num_hurt_frames
+		var random_position = Vector3.ZERO
+		random_position.x = randi_range(-WRAP_X, WRAP_X)
+		random_position.z = randi_range(-WRAP_Z, WRAP_Z)
+		self.pos = random_position
+		self.radius = p_breed.radius
 		self.seed = randf_range(0.0, 1.0)
-		self.speed = BADDIE_SPEED
+		self.speed = p_breed.speed
+
+class Breed:
+	var behavior_func
+	var color: Color
+	var hp: int
+	var num_hurt_frames: int
+	var radius: float
+	var speed: float
+
+	func _init(p_behavior_func, p_color: Color, p_hp: int, p_num_hurt_frames: int, p_radius: float, p_speed: float):
+		self.behavior_func = p_behavior_func
+		self.color = p_color
+		self.hp = p_hp
+		self.num_hurt_frames = p_num_hurt_frames
+		self.radius = p_radius
+		self.speed = p_speed
+
+func behavior_func_seek(p_a: Actor, p_player_pos: Vector3):
+	var dir: Vector3
+	dir.x = p_player_pos.x - p_a.pos.x
+	dir.z = p_player_pos.z - p_a.pos.z
+	p_a.pos += dir.normalized() * p_a.speed
+
+func behavior_func_wander(p_a: Actor, p_player_pos: Vector3):
+	p_a.pos.x += p_a.speed * sin(0.01 * frame + p_a.seed)
+
+func behavior_func_wander_then_seek(p_a: Actor, p_player_pos: Vector3):
+	if p_a.hp <= 1:
+		p_a.speed *= 2.0
+		behavior_func_seek(p_a, p_player_pos)
+		p_a.speed /= 2.0
+	else:
+		behavior_func_wander(p_a, p_player_pos)
 
 func is_overlapping(p1, r1, p2, r2):
 	return(p2.x - p1.x) * (p2.x - p1.x) + (p2.z - p1.z) * (p2.z - p1.z) < (r1 + r2) * (r1 + r2)
 
 func _ready() -> void:
-	player = Character.new(Vector3.ZERO, PLAYER_RADIUS, Color.DARK_OLIVE_GREEN, PLAYER_HP, PLAYER_HURT_FRAMES)
+	#RenderingServer.set_default_clear_color(Color(0.1, 0.1, 0.1))
 
-	sword_frame = PLAYER_SWORD_FRAMES + 1
+	var player_breed = Breed.new(behavior_func_seek, PLAYER_COLOR, PLAYER_HP, PLAYER_NUM_HURT_FRAMES, PLAYER_RADIUS, PLAYER_SPEED)
+	player = Actor.new(player_breed)
+	player.pos = Vector3(0, 0, 0*WRAP_Z)
+
+	sword_frame = PLAYER_NUM_SWORD_FRAMES + 1
 	sword = MeshInstance3D.new()
 	sword.mesh = BoxMesh.new()
 	sword.mesh.size = Vector3(PLAYER_SWORD_LENGTH, 0.1, 0.25)
 	var material = StandardMaterial3D.new()
 	material.albedo_color = Color.YELLOW
+	material.set_transparency(1) # enum not working
 	material.flags_unshaded = true
 	sword.mesh.surface_set_material(0, material)
 	sword_pivot = Node3D.new()
@@ -105,122 +162,143 @@ func _ready() -> void:
 	player.mesh_instance.add_child(sword_debug)
 	sword_debug.visible = false
 
-	for i in BADDIE_COUNT:
-			var random_position = Vector3.ZERO
-			random_position.x = randi_range(-WRAP_X, WRAP_X)
-			random_position.z = randi_range(-WRAP_Z, WRAP_Z)
-			baddie_list.append(Character.new(random_position, BADDIE_RADIUS, Color.DIM_GRAY, BADDIE_HP, BADDIE_HURT_FRAMES))
-	
+	var skull_breed = Breed.new(behavior_func_wander_then_seek, SKULL_COLOR, SKULL_HP, SKULL_NUM_HURT_FRAMES, SKULL_RADIUS, SKULL_SPEED)
+	for i in SKULL_COUNT:
+			baddie_list.append(Actor.new(skull_breed))
+
+	var bat_breed = Breed.new(behavior_func_seek, BAT_COLOR, BAT_HP, BAT_NUM_HURT_FRAMES, BAT_RADIUS, BAT_SPEED)
+	for i in BAT_COUNT:
+			baddie_list.append(Actor.new(bat_breed))
+
+	var root = get_tree().root.get_children()[0] # Switch to Engine.get_main_loop() (as SceneTree)... or actually call get_node("Root") and pass it for the Actor to attach
+
+	root.add_child(player.mesh_instance)
+
+	for i in baddie_list.size():
+		root.add_child(baddie_list[i].mesh_instance)
+
 	camera = Camera3D.new()
 	camera.set_orthogonal(20.0, 1.0, 40.0)
 	camera.look_at_from_position(Vector3(0, 20, 0), Vector3(0, 0, 0), Vector3(0, 0, -1))
 	camera.make_current()
 	camera_shake_frame = CAMERA_SHAKE_FRAMES + 1
-	
-	var root = get_tree().root.get_children()[0] # Switch to Engine.get_main_loop() (as SceneTree)... or actually call get_node("Root") and pass it for the Character to attach
-	root.add_child(player.mesh_instance)
-	for i in BADDIE_COUNT:
-		root.add_child(baddie_list[i].mesh_instance)
 	root.add_child(camera)
+	
 	
 func _process(_delta: float) -> void:
 	frame = frame + 1
 
-	is_attacking = Input.is_key_pressed(KEY_J)
-	if is_attacking && !was_attacking && sword_frame >= PLAYER_SWORD_FRAMES:
-		sword_frame = 0
-		sword_pivot.transform = Transform3D.IDENTITY
-		sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, PLAYER_SWORD_ATTACK_START_ANGLE)
-		player.pos.x += cos(player.theta) * PLAYER_ATTACK_ROOT_MOTION * -0.05
-		player.pos.z -= sin(player.theta) * PLAYER_ATTACK_ROOT_MOTION * -0.05
-		sword.get_active_material(0).albedo_color = Color.WHITE
+	if sword_frame > PLAYER_NUM_SWORD_FRAMES and player.hurt_frame > player.num_hurt_frames:
+		if Input.is_key_pressed(KEY_A): input_bit_field |= Direction.LEFT
+		if Input.is_key_pressed(KEY_D): input_bit_field |= Direction.RIGHT
+		if Input.is_key_pressed(KEY_W): input_bit_field |= Direction.UP
+		if Input.is_key_pressed(KEY_S): input_bit_field |= Direction.DOWN
 
-	was_attacking = is_attacking
+		var tmp_dir = Vector3i(0, 0, 0)
 
-	sword.transform.origin = Vector3(PLAYER_SWORD_LENGTH/1.5, 0.0, 0.0) # can i do this up top? or not until it is added to the scene?
+		if input_bit_field & Direction.LEFT:
+			input_bit_field ^= Direction.LEFT
+			tmp_dir.x -= 1
 
-	if sword_frame < PLAYER_SWORD_FRAMES:
-		sword.show()
+		if input_bit_field & Direction.RIGHT:
+			input_bit_field ^= Direction.RIGHT
+			tmp_dir.x += 1
 
-		match sword_frame:
-			0:
-				pass # set up above, on impact
-			_ when sword_frame < 5:
-				sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, 0.2 * PLAYER_SWORD_ATTACK_ANGLE_INCREMENT)
-				sword.get_active_material(0).albedo_color = Color.YELLOW
-				player.pos.x += cos(player.theta) * (PLAYER_ATTACK_ROOT_MOTION / PLAYER_SWORD_FRAMES) * 0.1
-				player.pos.z -= sin(player.theta) * (PLAYER_ATTACK_ROOT_MOTION / PLAYER_SWORD_FRAMES) * 0.1
-			_ when sword_frame < 9:
-				sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, 1.7 * PLAYER_SWORD_ATTACK_ANGLE_INCREMENT)
-				player.pos.x += cos(player.theta) * (PLAYER_ATTACK_ROOT_MOTION / PLAYER_SWORD_FRAMES)
-				player.pos.z -= sin(player.theta) * (PLAYER_ATTACK_ROOT_MOTION / PLAYER_SWORD_FRAMES)
-			_ when sword_frame < PLAYER_SWORD_FRAMES:
-				sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, 0.45 * PLAYER_SWORD_ATTACK_ANGLE_INCREMENT)
-				player.pos.x += cos(player.theta) * (PLAYER_ATTACK_ROOT_MOTION / PLAYER_SWORD_FRAMES) * 0.3
-				player.pos.z -= sin(player.theta) * (PLAYER_ATTACK_ROOT_MOTION / PLAYER_SWORD_FRAMES) * 0.3
+		if input_bit_field & Direction.UP:
+			input_bit_field ^= Direction.UP
+			tmp_dir.z -= 1
 
-		sword_frame = min(sword_frame + 1, PLAYER_SWORD_FRAMES)
-				
-	else:
-		sword_pivot.transform = Transform3D.IDENTITY
-		#figure out how to tilt sword
-		#sword_pivot.transform = sword_pivot.transform.rotated(Vector3(0.2, 0.1, 0.2), 0.5*PI)
-		sword.hide()
+		if input_bit_field & Direction.DOWN:
+			input_bit_field ^= Direction.DOWN
+			tmp_dir.z += 1
 
-	if sword_frame >= PLAYER_SWORD_FRAMES and player.hurt_frame > PLAYER_HURT_FRAMES:
-		is_moving_left = Input.is_key_pressed(KEY_A)
-		if is_moving_left:
-			player.pos.x -= PLAYER_SPEED
-			player.theta = PI
-			
-		is_moving_right = Input.is_key_pressed(KEY_D)
-		if is_moving_right:
-			player.pos.x += PLAYER_SPEED
-			player.theta = 0
-			
-		is_moving_up = Input.is_key_pressed(KEY_W)
-		if is_moving_up:
-			player.pos.z -= PLAYER_SPEED
-			player.theta = PI/2.0
-			
-		is_moving_down = Input.is_key_pressed(KEY_S)
-		if is_moving_down:
-			player.pos.z += PLAYER_SPEED
-			player.theta = -PI/2.0
+		player.pos += PLAYER_SPEED * tmp_dir
 
-	if player.hurt_frame <= PLAYER_HURT_FRAMES:
+		if tmp_dir != Vector3i.ZERO:
+			player.dir = tmp_dir
+	
+	if player.hurt_frame <= player.num_hurt_frames:
 		match player.hurt_frame:
 			1:
 				player.pos += player.knockback_dir * 0.40
-			_ when player.hurt_frame <= PLAYER_HURT_FRAMES:
+			_ when player.hurt_frame < player.num_hurt_frames:
 				player.pos += player.knockback_dir * 0.05
-		if player.hurt_frame % 2 == 1:
-			player.mesh_instance.get_active_material(0).albedo_color = Color.WHITE
-		else:
-			player.mesh_instance.get_active_material(0).albedo_color = Color.DARK_OLIVE_GREEN
+				if player.hurt_frame % 2 == 0:
+					RenderingServer.set_default_clear_color(Color.BLACK)
+					player.mesh_instance.get_active_material(0).albedo_color = Color.WHITE
+				else:
+					RenderingServer.set_default_clear_color(Color.RED)
+					player.mesh_instance.get_active_material(0).albedo_color = Color.BLACK
+			_ when player.hurt_frame == player.num_hurt_frames:
+				player.mesh_instance.get_active_material(0).albedo_color = Color.DARK_OLIVE_GREEN
+				RenderingServer.set_default_clear_color(Color(0.1, 0.1, 0.1))
+
+
 		player.hurt_frame += 1
 
 	player.pos.x = clamp(player.pos.x, -WRAP_X, WRAP_X)
 	player.pos.z = clamp(player.pos.z, -WRAP_Z, WRAP_Z)
 			
 	player.mesh_instance.transform.origin = player.pos
+
+	var player_theta = Vector2(player.dir.x, -player.dir.z).angle()
 	var m1 = Basis.IDENTITY
-	player.mesh_instance.transform.basis = m1.rotated(Vector3.UP, player.theta)
+	player.mesh_instance.transform.basis = m1.rotated(Vector3.UP, player_theta)
+
 	var breathing_scale = 0.9 + 0.1 * abs(pow(sin(0.02 * frame), 3))
 	player.mesh_instance.scale = Vector3(breathing_scale, 1.0, breathing_scale)
+
+	sword.transform.origin = Vector3(PLAYER_SWORD_LENGTH/1.5, 0.0, 0.0) # can i do this up top? or not until it is added to the scene?
 
 	if Input.is_key_pressed(KEY_H):
 		sword_debug.visible = !sword_debug.visible
 
+	is_attacking = Input.is_key_pressed(KEY_J)
+	if is_attacking && !was_attacking && sword_frame > PLAYER_NUM_SWORD_FRAMES:
+		sword_frame = 1
+
+	was_attacking = is_attacking
+
+	if sword_frame <= PLAYER_NUM_SWORD_FRAMES:
+		sword.show()
+
+		match sword_frame:
+			1:
+				sword_pivot.transform = Transform3D.IDENTITY
+				sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, PLAYER_SWORD_ATTACK_START_ANGLE)
+				player.pos += player.dir * PLAYER_SWORD_ROOT_MOTION * -0.15 # BUG!!! ONE FRAME BEHIND, SINCE WE UPDATE PLAYER ABOVE! BUT IF ITS DOWN HERE, FALSE POSITIVE ON SWIPE!!!
+				sword.get_active_material(0).albedo_color = Color.WHITE
+			_ when sword_frame < 7:
+				sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, 0.10 * PLAYER_SWORD_ATTACK_ANGLE_INCREMENT)
+				player.pos += player.dir * (PLAYER_SWORD_ROOT_MOTION / PLAYER_NUM_SWORD_FRAMES) * 0.0
+				sword.get_active_material(0).albedo_color = Color.YELLOW
+			_ when sword_frame < 11:
+				sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, 2.2 * PLAYER_SWORD_ATTACK_ANGLE_INCREMENT)
+				player.pos += player.dir * (PLAYER_SWORD_ROOT_MOTION / PLAYER_NUM_SWORD_FRAMES)
+				sword.get_active_material(0).albedo_color = Color(1, 1, 0, 0.27)
+			_ when sword_frame < PLAYER_NUM_SWORD_FRAMES:
+				sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, 0.65 * PLAYER_SWORD_ATTACK_ANGLE_INCREMENT)
+				player.pos += player.dir * (PLAYER_SWORD_ROOT_MOTION / PLAYER_NUM_SWORD_FRAMES) * 0.1
+				sword.get_active_material(0).albedo_color = Color.YELLOW
+			_ when sword_frame == PLAYER_NUM_SWORD_FRAMES:
+				sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, 0.40 * PLAYER_SWORD_ATTACK_ANGLE_INCREMENT)
+				sword.get_active_material(0).albedo_color = Color(1, 1, 0, 0.50)
+
+		sword_frame += 1
+				
+	else: 
+		sword_pivot.transform = Transform3D.IDENTITY
+		sword_pivot.transform = sword_pivot.transform.rotated(Vector3.UP, PLAYER_SWORD_ATTACK_START_ANGLE)
+		sword.hide()
+
 	sword_debug.transform.origin = Vector3.ZERO
-	sword_debug.transform.origin += sword_pivot.basis.x.normalized() * 0.8 * PLAYER_SWORD_LENGTH
-	sword.scale = Vector3(1, 1, 1)
+	sword_debug.transform.origin += sword_pivot.basis.x.normalized() * 0.9 * PLAYER_SWORD_LENGTH
 	
-	for i in BADDIE_COUNT:
+	for i in baddie_list.size():
 		var b = baddie_list[i]
 		b.hurt_frame += 1
 
-		if b.hurt_frame > BADDIE_HURT_FRAMES && is_overlapping(b.pos, b.radius, sword_debug.global_transform.origin, PLAYER_SWORD_DEBUG_SPHERE_SIZE) && sword_frame < PLAYER_SWORD_FRAMES:
+		if b.hurt_frame > b.num_hurt_frames && is_overlapping(b.pos, b.radius, sword_debug.global_transform.origin, PLAYER_SWORD_DEBUG_SPHERE_SIZE) && sword_frame <= PLAYER_NUM_SWORD_FRAMES:
 			b.hurt_frame = 1
 			b.knockback_dir.x = b.pos.x - player.pos.x
 			b.knockback_dir.z = b.pos.z - player.pos.z
@@ -230,11 +308,11 @@ func _process(_delta: float) -> void:
 				camera_shake_frame = 1
 			b.speed *= -1
 
-		if b.hurt_frame <= BADDIE_HURT_FRAMES:
+		if b.hurt_frame <= b.num_hurt_frames:
 
 			match b.hurt_frame:
 				1:
-					b.mesh_instance.get_active_material(0).albedo_color = Color.DIM_GRAY
+					b.mesh_instance.get_active_material(0).albedo_color = Color.SIENNA
 					b.mesh_instance.scale = Vector3(0.9, 0.9, 0.9)
 					b.pos += b.knockback_dir * -0.05
 					b.pos.x += randf_range(-1.0, 1.0) * 0.1
@@ -243,55 +321,55 @@ func _process(_delta: float) -> void:
 				2:
 					b.mesh_instance.get_active_material(0).albedo_color = Color.WHITE
 					b.mesh_instance.scale = Vector3(0.8, 0.8, 0.8)
-					b.pos += b.knockback_dir * -0.05
+					b.pos += b.knockback_dir * -0.03
 					b.pos.x += randf_range(-1.0, 1.0) * 0.09
 					b.pos.z += randf_range(-1.0, 1.0) * 0.09
 				3:
 					b.mesh_instance.get_active_material(0).albedo_color = Color.WHITE
 					b.mesh_instance.scale = Vector3(1.35, 1.35, 1.35)
-					b.pos += b.knockback_dir * 0.50
+					b.pos += b.knockback_dir * 0.55
 					b.pos.x += randf_range(-1.0, 1.0) * 0.08
 					b.pos.z += randf_range(-1.0, 1.0) * 0.08
 				4:
 					b.mesh_instance.get_active_material(0).albedo_color = Color.WHITE
 					b.mesh_instance.scale = Vector3(1.2, 1.2, 1.2)
-					b.pos += b.knockback_dir * 0.20
+					b.pos += b.knockback_dir * 0.35
 					b.pos.x += randf_range(-1.0, 1.0) * 0.07
 					b.pos.z += randf_range(-1.0, 1.0) * 0.07
 				5:
-					b.mesh_instance.get_active_material(0).albedo_color = Color.DIM_GRAY
+					b.mesh_instance.get_active_material(0).albedo_color = Color.SIENNA
 					b.mesh_instance.scale = Vector3(1.1, 1.1, 1.1)
-					b.pos += b.knockback_dir * 0.17
+					b.pos += b.knockback_dir * 0.22
 					b.pos.x += randf_range(-1.0, 1.0) * 0.06
 					b.pos.z += randf_range(-1.0, 1.0) * 0.06
-				_ when b.hurt_frame < BADDIE_HURT_FRAMES:
-					b.mesh_instance.get_active_material(0).albedo_color = Color.DIM_GRAY
+				_ when b.hurt_frame < b.num_hurt_frames:
+					b.mesh_instance.get_active_material(0).albedo_color = Color.SIENNA
 					b.mesh_instance.scale = Vector3(1, 1, 1)
 					b.pos.x += randf_range(-1.0, 1.0) * 0.01
 					b.pos.z += randf_range(-1.0, 1.0) * 0.01
-					b.pos += b.knockback_dir * 0.05
+					b.pos += b.knockback_dir * 0.10
 					if b.hp == 0:
 						b.pos = Vector3(999, 999, 999)
 						b.speed = 0
 						b.mesh_instance.hide()
 
-		if b.hurt_frame > BADDIE_HURT_FRAMES:
-			b.pos.x += b.seed * b.speed * sin(0.01 * frame + b.seed)
+		b.behavior_func.call(b, player.pos)
 
 		b.mesh_instance.transform.origin = b.pos
 
-		if b.hurt_frame > BADDIE_HURT_FRAMES && is_overlapping(b.pos, b.radius, player.pos, player.radius):
+		if is_overlapping(b.pos, b.radius, player.pos, player.radius) && sword_frame > PLAYER_NUM_SWORD_FRAMES:
 			player.hurt_frame = 1
 			player.knockback_dir.x = player.pos.x - b.pos.x
 			player.knockback_dir.z = player.pos.z - b.pos.z
 			player.knockback_dir = player.knockback_dir.normalized()
 			player.hp -= 1
+			camera_shake_frame = 1
 
 	if camera_shake_frame <= CAMERA_SHAKE_FRAMES:
 		camera.transform.origin.x = randf_range(-1.0, 1.0) * CAMERA_SHAKE_AMOUNT * (1.0 - float(camera_shake_frame)/CAMERA_SHAKE_FRAMES)
 		camera.transform.origin.z = randf_range(-1.0, 1.0) * CAMERA_SHAKE_AMOUNT * (1.0 - float(camera_shake_frame)/CAMERA_SHAKE_FRAMES)
-		var theta = randf_range(-PI, PI) * 0.03
-		camera.rotation_degrees.z += theta
+		var camera_theta = randf_range(-PI, PI) * 0.02
+		camera.rotation_degrees.z += camera_theta
 		camera_shake_frame += 1
 	else:
 		camera.look_at_from_position(Vector3(0, 20, 0), Vector3(0, 0, 0), Vector3(0, 0, -1))
